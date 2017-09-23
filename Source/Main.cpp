3d730a22684a4c7382e54f72f176e2530e236418
cybernik <cybernik@ubuntu>
2017-09-23 21:28:48 +0800

    implementing

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,7 +4,20 @@
  *  Created on: Sep 21, 2017
  *      Author: root
  */
+#include "Utils/ApplicationManager.h"
 
+int main (int argc, char* argv[])
+{
+	ApplicationManager manager;
 
+	if (manager.initialize () == false)
+	{
+		exit (-1);
+	}
+
+	manager.pending ();
+
+	exit(0);
+}
 
 
--- Source/Utils/ApplicationManager.cpp
+++ Source/Utils/ApplicationManager.cpp
@@ -14,21 +14,45 @@ static ApplicationManager* instance;
 
 ApplicationManager::ApplicationManager() {
 	instance = this;
+	applications.clear ();
 
 }
 
 ApplicationManager::~ApplicationManager() {
-	// TODO Auto-generated destructor stub
+
 }
 
 bool ApplicationManager::addApplication (ApplicationProperties& application)
 {
-
+	applications.push_back (application);
 }
 
 bool ApplicationManager::runApplication (ApplicationProperties& application)
 {
+	if (application.getPID () == 0)
+	{
+		pid_t pid = fork ();
 
+		if (pid == 0)
+		{
+			//child process
+			int result;
+			result = execlp (application.getRunScript ().c_str (), NULL, NULL, NULL);
+			if (result < 0)
+			{
+				exit (-1);
+			}
+
+		}else if (pid > 0)
+		{
+			//parent process
+			application.setPID (pid);
+		}else{
+			//failure
+			return false;
+		}
+	}
+	return true;
 }
 
 bool ApplicationManager::runApplications ()
@@ -43,7 +67,27 @@ bool ApplicationManager::killApplicationByName (std::string app_name)
 
 bool ApplicationManager::killApplicationByPID (pid_t pid)
 {
-
+	if (pid != 0)
+	{
+		if (kill (pid, SIGTERM) != 0)
+		{
+			return false;
+		}else{
+			sleep (1);
+			int result = waitpid (pid, NULL, WNOHANG);
+			if (result == pid)
+			{
+				return true;
+			}else if (result == 0)
+			{
+				kill (pid, SIGKILL);
+				return true;
+			}else
+			{
+				return false;
+			}
+		}
+	}
 }
 
 bool ApplicationManager::killApplications ()
@@ -53,12 +97,19 @@ bool ApplicationManager::killApplications ()
 
 void ApplicationManager::signalChild (int no)
 {
+	int status;
+	pid_t pid;
 
+	while (((pid = waitpid(-1, &status, WNOHANG)) > 0) && running)
+	{
+		sleep(1);
+		instance->onApplicationQuit(pid, status);
+	}
 }
 
 void ApplicationManager::signalTerm (int no)
 {
-
+	running = false;
 }
 
 void ApplicationManager::registerSignals ()
@@ -84,4 +135,5 @@ void ApplicationManager::pending ()
 	{
 		rate.sleep ();
 	}
+	killApplications ();
 }
